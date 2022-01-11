04450cb6c65a57ec56e3b7a6a2bc28ee5602ab69
Mwapsam <mwapsam@gmail.com>
2022-01-11 23:32:45 +0200

    Complete project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,60 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const listData = [
+  {
+    description: 'Shopping',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Coding',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Learning',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Dancing',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Praying',
+    completed: false,
+    index: 3,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'ahmed'], ' ');
-  element.classList.add('hello');
+let listMarkUp = `
+        <div class="row title">
+            <p>Today's To Do</p>
+            <i class="fas fa-sync"></i>
+        </div>
+        <div class="row">
+            <input class="add-input" type="text" placeholder="Add to your list">
+            <i class="fas fa-level-down-alt rotate"></i>
+        </div>
+`;
 
-  return element;
-}
+listData.forEach((listItem) => {
+  listMarkUp += `
+        <div class="row">
+            <input type="checkbox">
+            <p>${listItem.description}</p>
+            <i class="fas fa-ellipsis-v"></i>
+        </div>
+        
+  `;
+});
 
-document.body.appendChild(component());
+listMarkUp += `
+        <div class="row clear">
+            <p>Clear All Completed</p>
+        </div>
+`;
+
+const listElement = document.querySelector('.list');
+listElement.innerHTML = listMarkUp;
