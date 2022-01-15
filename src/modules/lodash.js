b2bf366731f2e3453ed4fd966478da26b2246d73
Mwapsam <mwapsam@gmail.com>
2022-01-15 14:48:23 +0200

    add interactive feature

--- src/modules/lodash.js
+++ src/modules/lodash.js
@@ -4,7 +4,6 @@ import _ from 'lodash';
 export function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
   element.innerHTML = _;
 
   return element;
